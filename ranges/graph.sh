4b87bbe08fd5dddb7c9c56a241c2cc0b08609661
Eric Richardson <ejricha+github@gmail.com>
2019-12-03 18:47:08 -0500

    Fix the range concepts hierarchy

--- ranges/graph.sh
+++ ranges/graph.sh
@@ -10,46 +10,46 @@ SVG=$B.svg
 # Create the .dot file
 cat << EOF > $DOT
 strict digraph "Concepts" {
-	node [ fontname = "Ubuntu Mono" fontstyle = "bold" fontsize = "12" shape = "polygon" color = "blue" penwidth = 3 ];
+	node [ fontname = "monospace" fontsize = "12" shape = "polygon" color = "blue" penwidth = 3 ];
 	edge [ color = "black" penwidth = 2 ]
 	range [ shape = "house" ];
 	range -> common_range
+	sized [ label = "sized_range\ndisable_sized_range" ];
+	range -> sized
 	range -> output_range
-	range -> sized_range
-	range -> disabled_size_range
-	subgraph {
-		rank="same"
-		common_range
-		view
-		forward_range
-	}
-	subgraph {
-		rank="same"
-		output_range
-		enable_view
-		bidirectional_range
-	}
-	subgraph {
-		rank="same"
-		sized_range
-		view_base
-		random_access_range
-	}
-	subgraph {
-		rank="same"
-		disabled_size_range
-		viewable_base
-		contiguous_range
-	}
+	view [ label = "view\nenable_view\nview_base" ];
 	range -> view
-	range -> enable_view
-	range -> view_base
-	view_base -> viewable_base
+	view -> viewable_range
 	range -> input_range
 	input_range -> forward_range
 	forward_range -> bidirectional_range
 	bidirectional_range -> random_access_range
 	random_access_range -> contiguous_range
+	subgraph {
+		rank="same"
+		common_range
+		input_range
+	}
+	subgraph {
+		rank="same"
+		sized
+		forward_range
+	}
+	subgraph {
+		rank="same"
+		output_range
+		bidirectional_range
+	}
+	subgraph {
+		rank="same"
+		view
+		random_access_range
+	}
+	subgraph {
+		rank="same"
+		viewable_range
+		contiguous_range
+	}
 }
 EOF
 
